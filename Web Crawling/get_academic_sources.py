0b5d1200580d0bf0286c2fbfac498ec5934587f8
Raissa Bergamini <raissa.bergamini@ext.vallourec.com>
2018-04-11 19:59:43 -0300

    finished link springer crawler

--- Web Crawling/get_academic_sources.py
+++ Web Crawling/get_academic_sources.py
@@ -44,37 +44,36 @@ def findGoogleScholarReferences(key = "test",nPages = 2, file = "googleAcademicO
 
 def findLinkSpringerReferences(key = "test",nPages = 2, file = "linkSpringerOutput.json"):
     extractedReferences = []
-    
+    nPages = 2
+    file = "linkSpringerOutput.json"
     for page in range(nPages):
         url = "https://link-springer-com.ez27.periodicos.capes.gov.br/search/page/"+ str(page) +"?query="+ key.replace(" ", "+") 
-url = "https://link-springer-com.ez27.periodicos.capes.gov.br/search/page/1?query=multivariate+time+series+forecasting"
-
-page = requests.get(url)
-
-if(page.status_code != 200):
-    return(dict([("references", []),("connection", 'failed')]))
+        url = "https://link-springer-com.ez27.periodicos.capes.gov.br/search/page/1?query=multivariate+time+series+forecasting"
         
-soup = BeautifulSoup(page.content, 'html.parser')
-expectedReferences = soup.find_all('div', class_='gs_r gs_or gs_scl')
-
-
-for reference in expectedReferences:
-    soup2 = BeautifulSoup(str(reference), 'html.parser')
-    try:
-        a = soup2.find_all('h3', class_='gs_rt')[0]
-        title = a.find_all('a')[0].get_text()
-        url = a.find_all('a', href=True)[0]['href']
-        author = soup2.find_all('div', class_='gs_a')[0].get_text()
-        description = soup2.find_all('div', class_='gs_rs')[0].get_text()
-        extractedReferences.append(dict([("title", title),("url", url),("author", author),("description", description)]))
-    except:
-        #print('error')
-        pass
-            
+        page = requests.get(url)
+        
+        if(page.status_code != 200):
+            return(dict([("references", []),("connection", 'failed')]))
+                
+        soup = BeautifulSoup(page.content, 'html.parser')
+        expectedReferences = soup.find('ol').find_all('li')
+        
+        for reference in expectedReferences:
+            try:
+                title = reference.find('a', class_='title').get_text()
+                url = reference.find('a', class_='title')['href']
+                author = reference.find('span', class_='authors').get_text().replace('\n',' ').replace('  ','')
+                description = reference.find('p', class_='snippet').get_text().replace('\n',' ').replace('  ','')
+                extractedReferences.append(dict([("title", title),
+                                                 ("url",  'https://link-springer-com.ez27.periodicos.capes.gov.br' + url),
+                                                 ("author", author),
+                                                 ("description", description)]))
+            except:
+                #print('error')
+                pass
+                        
     storeInJson(extractedReferences,file)
     return(dict([("references", extractedReferences),("source", "linkSpringer"),("connection", 'success')]))
-
-
 
 #downloading and converting to JSON
 def storeInJson(extractedReferences, file):
