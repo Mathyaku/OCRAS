82909e22aca59feea47fa58907a7ee442d2ffe14
Pedro <pedbrgs@gmail.com>
2018-04-11 21:34:49 -0300

    Melhorias e integração

--- OCR/ocr.py
+++ OCR/ocr.py
@@ -7,6 +7,10 @@ import pytesseract
 import PythonMagick as pm
 import argparse
 import cv2
+import os
+import deskew
+import improvement
+import binarisation
 
 # Localizando o modulo
 pytesseract.pytesseract.tesseract_cmd = "C:/Program Files (x86)/Tesseract-OCR/tesseract.exe"
@@ -17,16 +21,18 @@ ap.add_argument("-i", "--image", required=True,	help="path to input image file")
 ap.add_argument("-o", "--output", required=True, help="path to output image file")
 args = vars(ap.parse_args())
  
+ 
 # Carregando imagem
 img = pm.Image(args["image"])
 
-# Convertendo a imagem para dois canais de cores
-img.colorSpace(pm.ColorspaceType.GRAYColorspace)
-img.quality(100)
-img.sharpen(50, 50)
+# Rotacionando imagem
+deskew.rotateImage(args["image"],args["output"])
 
-# Salvando imagem convertida
-img.write(args["output"])
+# Aumento da qualidade da imagem
+improvement.imageSharpness(args["output"])
+
+# Binarização e remoção de ruídos
+binarisation.noiseRemoval(args["output"])
 
 # Extraindo o texto da imagem
 print( pytesseract.image_to_string(Image.open(args["output"]), lang = 'por')) 
