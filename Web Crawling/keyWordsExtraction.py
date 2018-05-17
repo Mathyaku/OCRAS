7fbd9701a0f1163d4671fc7806ab1f82c8fbbd99
mathyaku <mathyaku@gmail.com>
2018-05-16 21:38:59 -0300

    simple key word extraction

---
+++ Web Crawling/keyWordsExtraction.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  9 19:51:43 2018
+
+@author: Matheus
+"""
+
+from rake_nltk import Rake
+
+def GetRelevanteKeyWords(text, language):
+    
+    r = Rake(language=language)
+    
+    r.extract_keywords_from_text(text)
+    
+    r.get_ranked_phrases() # To get keyword phrases ranked highest to lowest.
+    
+    
+    
+    r.get_word_frequency_distribution()
+    
+    teste = r.get_word_degrees()
+    
+    
+    
+    keyWordsSorted = sorted(teste.items(), key=lambda x: x[1], reverse = True)
+    
+    keyWordsRelevante = [x[0] for x in keyWordsSorted[0:5]]
+    
+    return {'keywords': keyWordsRelevante, 'result': keyWordsSorted}
+
+
+
+text = "cry The Project MATHEUS Matheus Gutenberg EBook of  a a a a a a cry matheus Crime and Punishment, cry by  MATHEUS Matheus Fyodor Dostoevsky\r\n"
+keyWordsExtraction = GetRelevanteKeyWords(text, "english")  # portuguese or english
+
+keyWordsExtraction['keywords']
+keyWordsExtraction['result']
